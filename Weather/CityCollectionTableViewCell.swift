2078e5680bf35c0bfc4d02a4442030c293af8848
ningmengjean <ningmengjean@gmail.com>
2017-05-26 13:44:27 +0800

    add newfeature

---
+++ Weather/CityCollectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityCollectionTableViewCell.swift
+//  Weather
+//
+//  Created by wangchi on 2017/5/24.
+//  Copyright © 2017年 Zhu xiaojin. All rights reserved.
+//
+
+import UIKit
+
+class CityCollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
